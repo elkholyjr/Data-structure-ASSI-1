57d18961775ea2e5cab33124a88eaaca4bc402cf
omar osama <omarosos912@gmail.com>
2024-04-21 18:05:39 +0200

    added shell sort

---
+++ phase 1/ShellSort/shellSort.cpp
@@ -0,0 +1,157 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <chrono>
+using namespace std;
+using namespace chrono;
+
+class student
+{
+private:
+    int id;
+    string name;
+    double gpa;
+
+public:
+    student(int id, string name, double gpa) : id(id), name(name), gpa(gpa) {}
+
+    int getId() const
+    {
+        return id;
+    }
+
+    string getName() const
+    {
+        return name;
+    }
+
+    double getGpa() const
+    {
+        return gpa;
+    }
+
+    bool operator<(const student &other) const
+    {
+        if (name == other.name)
+        {
+            return gpa < other.gpa;
+        }
+        return name < other.name;
+    }
+};
+
+template <typename T>
+void shellSort(vector<T> &arr, int &comparisons)
+{
+    int n = arr.size();
+    for (int gap = n / 2; gap > 0; gap /= 2)
+    {
+        for (int i = gap; i < n; i++)
+        {
+            T temp = arr[i];
+            int j;
+            for (j = i; j >= gap && !(arr[j - gap] < temp); j -= gap)
+            {
+                arr[j] = arr[j - gap];
+                comparisons++;
+            }
+            arr[j] = temp;
+        }
+    }
+}
+
+template <typename T, typename Compare>
+void shellSort(vector<T> &arr, int &comparisons, Compare compare)
+{
+    int n = arr.size();
+    for (int gap = n / 2; gap > 0; gap /= 2)
+    {
+        for (int i = gap; i < n; i++)
+        {
+            T temp = arr[i];
+            int j;
+            for (j = i; j >= gap && compare(arr[j - gap], temp); j -= gap)
+            {
+                arr[j] = arr[j - gap];
+                comparisons++;
+            }
+            arr[j] = temp;
+        }
+    }
+}
+
+int main()
+{
+    vector<student> students;
+
+    ifstream file("../students.txt");
+    if (!file)
+    {
+        cout << "Error opening file." << endl;
+        return 1;
+    }
+
+    int nameComparisons{0};
+    int gpaComparisons{0};
+
+    int numStudents;
+    file >> numStudents;
+    file.ignore();
+
+    for (int i = 0; i < numStudents; ++i)
+    {
+        string name, idStr, gpaStr;
+        getline(file, name);
+        getline(file, idStr);
+        getline(file, gpaStr);
+
+        students.emplace_back(stoi(idStr), name, stod(gpaStr));
+    }
+
+    ofstream outputByName("SortedByName.txt");
+    ofstream outputByGPA("SortedByGPA.txt");
+
+    auto start = high_resolution_clock::now();
+    // Sorting by name || insertion sort
+    shellSort(students, nameComparisons);
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+
+    outputByName << "Algorithm: Count Sort\n";
+    outputByName << "Number of comparisons:" << nameComparisons << "\n";
+    outputByName << "Running Time: " << duration.count() << " milliseconds\n";
+    outputByName << "Sorted by Name:\n";
+    for (const auto &s : students)
+    {
+        outputByName << s.getName() << "\n"
+                     << s.getId() << "\n"
+                     << s.getGpa() << "\n";
+    }
+    outputByName << endl;
+
+    start = high_resolution_clock::now();
+    // Sorting by GPA || insertion sort
+    shellSort(students, gpaComparisons, [](const student &s1, const student &s2) -> bool
+              { return s1.getGpa() < s2.getGpa(); });
+    stop = high_resolution_clock::now();
+    duration = duration_cast<milliseconds>(stop - start);
+
+    outputByGPA << "Algorithm: Quick Sort\n";
+    outputByGPA << "Number of comparisons:" << gpaComparisons << "\n";
+    outputByGPA << "Running Time: " << duration.count() << " milliseconds\n";
+    outputByGPA << "Sorted by GPA:\n";
+    for (const auto &s : students)
+    {
+        outputByGPA << s.getName() << "\n"
+                    << s.getId() << "\n"
+                    << s.getGpa() << "\n";
+    }
+    outputByGPA << endl;
+
+    outputByName.close();
+    outputByGPA.close();
+
+    return 0;
+}
